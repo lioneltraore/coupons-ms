36ae721e41d3caa173551702612f2dce9ce28d5b
Traore Leonel <ltraore@m2i-sa.com>
2024-07-30 05:46:11 +0000

    first commit

---
+++ src/main/java/com/cubixroot/courses/coupon/CouponMsApplication.java
@@ -0,0 +1,13 @@
+package com.cubixroot.courses.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponMsApplication.class, args);
+	}
+
+}
